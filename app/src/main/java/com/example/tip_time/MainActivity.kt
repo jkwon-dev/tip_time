0a8daf0c6e842de023183d94d8311e26de64ab1d
jkwon-dev <kwonejk@gmail.com>
2022-12-15 20:31:33 +0800

    Add a tip percentage text field

--- app/src/main/java/com/example/tip_time/MainActivity.kt
+++ app/src/main/java/com/example/tip_time/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tip_time
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
@@ -21,8 +22,8 @@ import androidx.compose.ui.unit.sp
 import com.example.tip_time.ui.theme.Tip_timeTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import java.text.NumberFormat
 
@@ -47,9 +48,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeScreen() {
     var amountInput by remember { mutableStateOf("0") }
+    var tipInput by remember { mutableStateOf("") }
 
     val amount = amountInput.toDoubleOrNull() ?: 0.0   //null이면 0.0으로 ?: Elvis 연산자
-    val tip = calculateTip(amount)
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount, tipPercent)
+
 
     Column(
         modifier = Modifier.padding(32.dp),
@@ -62,8 +66,22 @@ fun TipTimeScreen() {
         )
         Spacer(modifier = Modifier.height(16.dp))
         EditNumberField(
+            label = R.string.bill_amount,
+            keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+            ),
             value = amountInput,
             onValueChange = { amountInput = it}
+        )
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            value = tipInput,
+            onValueChange = { tipInput = it}
         )
         Spacer(modifier = Modifier.height(24.dp))
         Text(
@@ -77,16 +95,19 @@ fun TipTimeScreen() {
 
 @Composable
 fun EditNumberField(
+    @StringRes label: Int,            //label 매개변수가 문자열 리소스 참조여야 함을 나타내기 위해
+    keyboardOptions: KeyboardOptions,
     value: String,
-    onValueChange: (String) -> Unit
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     TextField(
         value = value,
         onValueChange = onValueChange,
-        label = { Text(text = stringResource(id = R.string.cost_of_service))},
+        label = { Text(text = stringResource(id = label))},
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,    //여러 줄에서 가로로 스크롤 가능한 하나의 줄로 압축됨.
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        keyboardOptions = keyboardOptions
     )
 }
 
